4e2787a5fcee93dd5c793ec1207b75f702a1521f
Nathan <nathanmolson@gmail.com>
2023-12-14 10:41:14 -0800

    create N copies of each glyph

--- build/export_glyphs.py
+++ build/export_glyphs.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python3
 import subprocess
+import random
+import numpy as np
 import fontforge
 import cairosvg
+import cv2 as cv
+
+N = 10
+
+VSHIFT_STD = 5
+HSHIFT_STD = 5
+
+def modify_glyph(img):
+    shift_x = HSHIFT_STD*np.random.randn()
+    shift_y = VSHIFT_STD*np.random.randn()
+    T = np.float32([ [1,0,shift_x], [0,1,shift_y] ])   
+    img_mod = cv.warpAffine(img, T, (img.shape[1], img.shape[0]), borderValue=255)
+    return img_mod
+
 f = fontforge.open("../TT.sfd")
 f.ascent+=300
 f.descent+=300
@@ -112,3 +128,9 @@ for gn in glyphnames:
     g.export(svg_path)
     cairosvg.svg2png(url=svg_path, write_to=png_path, dpi=600)
     subprocess.run(["convert", png_path, "-channel", "A", "-negate", "-separate", png_path])
+    img = cv.imread(png_path, cv.IMREAD_UNCHANGED)
+    for i in range(N):
+        if i >= 1:
+            png_path = f"pngs/{gn}.{i}.png"
+            img_mod = modify_glyph(img)
+            cv.imwrite(png_path, img_mod)
