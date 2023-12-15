e618bbd35c27f5f68a577856d0e90c937e12b7ca
Nathan <nathanmolson@gmail.com>
2023-12-15 07:58:10 -0800

    refactor

--- build/create_font.py
+++ build/create_font.py
@@ -1,32 +1,247 @@
-﻿import glob
+﻿#!/usr/bin/env python3
+import subprocess
+import glob
 import os
+from dataclasses import dataclass
+import numpy as np
+import matplotlib.pyplot as plt
 import fontforge
-
-f_base = fontforge.open("../TT.sfd")
-f = fontforge.font()
-
-f.ascent = f_base.ascent
-f.descent = f_base.descent
-f.em = f.ascent + f.descent
-f.uwidth = f_base.uwidth
-f.upos = f_base.upos
+import cairosvg
+import cv2 as cv
 
 
-for path in glob.glob("pngs/*.png"):
-    name = os.path.splitext(os.path.basename(path))[0]
-    print(path, name)
-    g = f.createChar(-1, name)
-    g.clear()
-    try:
-        g.importOutlines(path)
-    except:
-        print(f"Failed to import {g.glyphname}")
-        continue
-    g.activeLayer = 1
-    g.autoTrace()
-    g.clear(0)
-    # 237: 10 built in to svg export, 227 introduced by expanging images to IMG_WIDTH=1001
-    g.nltransform("x - 237", "y")
-    g.width = 547
+@dataclass
+class FontConfig:
+    base_font = "../TT.sfd"
+    num_alts = 1
+    const_vshift_std = 5.0
+    const_hshift_std = 5.0
+    vshift_std = 5.0
+    hshift_std = 25.0
+    blur = 3.0
+    noise = 256.0
+    ink_frac = 0.75
+    ink_std = 0.08
+    pressure_std = 0.08
+    bold_repeats = 2
+    font_name = "NoJo"
 
-f.save("test.sfd")
+
+IMG_WIDTH = 1001
+ORIG_GLYPH_DIR = "orig_pngs"
+
+GLYPHNAMES = ["space",
+              "exclam",
+              "quotedbl",
+              "numbersign",
+              "dollar",
+              "percent",
+              "ampersand",
+              "quotesingle",
+              "parenleft",
+              "parenright",
+              "asterisk",
+              "plus",
+              "comma",
+              "hyphen",
+              "period",
+              "slash",
+              "zero",
+              "one",
+              "two",
+              "three",
+              "four",
+              "five",
+              "six",
+              "seven",
+              "eight",
+              "nine",
+              "colon",
+              "semicolon",
+              "less",
+              "equal",
+              "greater",
+              "question",
+              "at",
+              "A",
+              "B",
+              "C",
+              "D",
+              "E",
+              "F",
+              "G",
+              "H",
+              "I",
+              "J",
+              "K",
+              "L",
+              "M",
+              "N",
+              "O",
+              "P",
+              "Q",
+              "R",
+              "S",
+              "T",
+              "U",
+              "V",
+              "W",
+              "X",
+              "Y",
+              "Z",
+              "bracketleft",
+              "backslash",
+              "bracketright",
+              "asciicircum",
+              "underscore",
+              "grave",
+              "a",
+              "b",
+              "c",
+              "d",
+              "e",
+              "f",
+              "g",
+              "h",
+              "i",
+              "j",
+              "k",
+              "l",
+              "m",
+              "n",
+              "o",
+              "p",
+              "q",
+              "r",
+              "s",
+              "t",
+              "u",
+              "v",
+              "w",
+              "x",
+              "y",
+              "z",
+              "braceleft",
+              "bar",
+              "braceright",
+              "asciitilde"]
+
+
+def modify_glyph(img, shift, config: FontConfig):
+    T = np.float32([[1, 0, shift[0]], [0, 1, shift[1]]])
+    img_mod = cv.warpAffine(
+        img, T, (img.shape[1], img.shape[0]), borderValue=255)
+
+    noise = np.random.normal(0, config.noise, img.shape)
+
+    ink_frac = np.random.normal(config.ink_frac, config.pressure_std)
+    ink_pad = np.random.normal(ink_frac, config.ink_std, (4, 6))
+    ink_pad = cv.resize(ink_pad, (img_mod.shape[1], img_mod.shape[0]))
+    if ink_frac <= 1:
+        img_mod = (255 - (255 - img_mod) * ink_pad) + noise
+    else:
+        img_blur = cv.GaussianBlur(img_mod, (0, 0), (ink_frac - 1)*100)
+        img_mod = ((np.minimum(img, img_blur)/255)**ink_pad)*255 + noise
+
+    img_mod = cv.GaussianBlur(img_mod, (0, 0), config.blur)
+
+    img_mod = ((img_mod > 128)*255).astype(np.uint8)
+
+    return img_mod.astype(np.uint8)
+
+
+def create_orig_pngs(config: FontConfig):
+    f_base = fontforge.open(config.base_font)
+
+    for gn in GLYPHNAMES:
+        g = f_base[gn]
+        g.width = f_base["M"].width
+        svg_path = f"svgs/{gn}.svg"
+        png_path = f"{ORIG_GLYPH_DIR}/{gn}.png"
+        g.unlinkRef()
+        g.export(svg_path)
+        cairosvg.svg2png(url=svg_path, write_to=png_path, dpi=600)
+        subprocess.run(["convert", png_path, "-channel", "A",
+                        "-negate", "-separate", png_path])
+
+
+def make_glyphs(config: FontConfig, bold: bool, italic: bool):
+    underscore_shift_x = config.const_hshift_std*np.random.randn()
+    underscore_shift_y = config.const_vshift_std*np.random.randn()
+    underscore_img = cv.imread(
+        f"{ORIG_GLYPH_DIR}/underscore.png", cv.IMREAD_UNCHANGED)
+
+    for gn in GLYPHNAMES:
+        orig_path = f"{ORIG_GLYPH_DIR}/{gn}.png"
+
+        if gn == "underscore":
+            shift_x = underscore_shift_x
+            shift_y = underscore_shift_y
+        else:
+            shift_x = config.const_hshift_std*np.random.randn()
+            shift_y = config.const_vshift_std*np.random.randn()
+
+        img = cv.imread(orig_path, cv.IMREAD_UNCHANGED)
+
+        T = np.float32([[1, 0, (IMG_WIDTH - img.shape[1])/2], [0, 1, 0]])
+        img = cv.warpAffine(
+            img, T, (IMG_WIDTH, img.shape[0]), borderValue=255)
+        for i in range(config.num_alts):
+            if i >= 1:
+                out_path = f"pngs/{gn}.{i}.png"
+            else:
+                out_path = f"pngs/{gn}.png"
+            img_mod = np.zeros_like(img)
+            repeats = config.bold_repeats if bold else 1
+            for i in range(repeats):
+                shift = (shift_x + config.hshift_std*np.random.randn(),
+                         shift_y + config.vshift_std*np.random.randn())
+                img_mod += (255 - modify_glyph(img, shift, config))
+            if italic and gn != "underscore":
+                shift = (underscore_shift_x + config.hshift_std*np.random.randn(),
+                         underscore_shift_y + config.vshift_std*np.random.randn())
+                img_mod += (255 - modify_glyph(underscore_img, shift))
+
+            cv.imwrite(out_path, (255 - img_mod).astype(np.uint8))
+
+
+def create_font_from_pngs(config):
+    f_base = fontforge.open("../TT.sfd")
+    f = fontforge.font()
+
+    f.ascent = f_base.ascent
+    f.descent = f_base.descent
+    f.em = f.ascent + f.descent
+    f.uwidth = f_base.uwidth
+    f.upos = f_base.upos
+
+    for path in glob.glob("pngs/*.png"):
+        name = os.path.splitext(os.path.basename(path))[0]
+        print(path, name)
+        g = f.createChar(-1, name)
+        g.clear()
+        try:
+            g.importOutlines(path)
+        except:
+            print(f"Failed to import {g.glyphname}")
+            continue
+        g.activeLayer = 1
+        g.autoTrace()
+        g.clear(0)
+        # 237: 10 built in to svg export, 227 introduced by expanging images to IMG_WIDTH=1001
+        shift = 10 + (IMG_WIDTH - f_base["M"].width)/2
+        g.nltransform(f"x - {shift}", "y")
+        g.width = f_base["M"].width
+
+    f.save(f"{config.font_name}.sfd")
+
+
+def create_font(config: FontConfig):
+    create_orig_pngs(config)
+    make_glyphs(config, bold=False, italic=False)
+    create_font_from_pngs(config)
+
+
+configs = [FontConfig()]
+for config in configs:
+    create_font(config)
